3c9d69678a023ba213392b5120ab71e32beed78f
Tabitha Mutinda <79199405+Tabithamutinda@users.noreply.github.com>
2022-12-14 12:01:08 +0300

    Displaying the chirps sent and displaying relative dates

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -17,7 +17,7 @@ class ChirpController extends Controller
     {
         //
         return Inertia::render('Chirps/Index',[
-
+            'chirps' => Chirp::with('user:id,name')->latest()->get(),
         ]);
     }
 
