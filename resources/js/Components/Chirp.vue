15f64cde30ecd78639b8015a1f15ed7005b29b5e
Tabitha Mutinda <79199405+Tabithamutinda@users.noreply.github.com>
2022-12-14 12:23:26 +0300

    Deleting a chirp

--- resources/js/Components/Chirp.vue
+++ resources/js/Components/Chirp.vue
@@ -4,6 +4,7 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 import Dropdown from '@/Components/Dropdown.vue';
 import InputError from '@/Components/InputError.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
+import DropdownLink from '@/Components/DropdownLink.vue';
 import { useForm } from '@inertiajs/inertia-vue3';
 import { ref } from 'vue';
  
@@ -43,6 +44,10 @@ const editing = ref(false);
                         <button class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:bg-gray-100 transition duration-150 ease-in-out" @click="editing = true">
                             Edit
                         </button>
+                                                <DropdownLink as="button" :href="route('chirps.destroy', chirp.id)" method="delete">
+                            Delete
+                        </DropdownLink>
+
                     </template>
                 </Dropdown>
                 </div>
